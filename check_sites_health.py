00cb6dc12a9f223433475c9f94d575f6d2cd40fb
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-29 15:45:48 +0400

    Split code

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,5 +1,5 @@
 import requests
-import whois
+import ph4whois
 import sys
 from urllib.parse import urlparse
 from prettytable import PrettyTable
@@ -12,42 +12,43 @@ def load_urls4check(path):
         return [line.strip() for line in file.readlines()]
 
 
-def is_server_respond_with_200(url):
-    return requests.get(url).status_code == 200
+def is_server_respond_with_200(url, notes=[]):
+    try:
+        return requests.get(url).ok
+    except requests.exceptions.ConnectionError:
+        notes.append('Error with connection to server')
+    except requests.exceptions.InvalidSchema:
+        notes.append('Invalid schema')
+    return False
 
 
 def get_domain_expiration_date(domain_name):
-    return whois.query(domain_name).get('expiration_date')
+    whois_info = ph4whois.whois(domain_name)
+    return whois_info.expiration_date
 
 
 def get_domain_from_url(url):
     return urlparse(url).hostname
 
 
-def get_url_info(url):
-    PREPAID_DAYS_LIMIT = 30
-    critical_prepaid_date = datetime.now() + timedelta(days=PREPAID_DAYS_LIMIT)
-
-    notes = []
-    domain = get_domain_from_url(url)
-
+def check_if_is_safety_prepaid(domain, notes=[]):
+    prepaid_days_limit = 30
+    critical_prepaid_date = datetime.now() + timedelta(days=prepaid_days_limit)
     try:
         expiration_date = get_domain_expiration_date(domain)
-        is_safety_prepaid = expiration_date >= critical_prepaid_date
-    except Exception:
-        is_safety_prepaid = False
+        return expiration_date < critical_prepaid_date
+    except AttributeError:
         notes.append('Error with connection to whois server')
-    except PrettyTable:
-        print('x')
+    except TypeError:
+        notes.append('Error with connection to whois server')
+    return False
 
-    try:
-        is_status_200 = is_server_respond_with_200(url)
-    except requests.exceptions.ConnectionError:
-        is_status_200 = False
-        notes.append('Error with connection to server')
-    except requests.exceptions.InvalidSchema:
-        notes.append('Invalid schema')
-        is_status_200 = False
+
+def get_url_info(url):
+    notes = []
+    domain = get_domain_from_url(url)
+    is_safety_prepaid = check_if_is_safety_prepaid(domain, notes=notes)
+    is_status_200 = is_server_respond_with_200(url, notes=notes)
 
     return [url, domain, is_safety_prepaid, is_status_200, notes]
 
@@ -70,7 +71,7 @@ if __name__ == '__main__':
 
     for url in domains_list:
         [url, domain, is_safety_prepaid,
-            is_status_200, notes] = get_url_info(url)
+         is_status_200, notes] = get_url_info(url)
         table.add_row([
             url,
             domain,
