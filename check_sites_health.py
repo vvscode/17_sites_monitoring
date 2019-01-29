341208954d13e43d68633840a120ff320c6f84c2
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-29 14:57:32 +0400

    Use argparse for gettting param

--- check_sites_health.py
+++ check_sites_health.py
@@ -3,6 +3,7 @@ import whois
 import sys
 from urllib.parse import urlparse
 from prettytable import PrettyTable
+import argparse
 
 def load_urls4check(path):
     with open(path) as file:
@@ -18,18 +19,19 @@ def get_domain_from_url(url):
     return urlparse(url).hostname
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        sys.exit('Please pass filename as a pram')
+    parser = argparse.ArgumentParser(description='Sites monitor')
+    parser.add_argument('file_path', help='Path to file with urls')
+    args = parser.parse_args()
 
     try:
-        domains_list = load_urls4check(sys.argv[1])
+        domains_list = load_urls4check(args.file_path)
     except FileNotFoundError:
         sys.exit('Pleas pass correct file name')
 
     print('List contains {} domain(s)'.format(len(domains_list)))
-    x = PrettyTable()
-    x.field_names = ["URL", "Domain", "Prepaid for a month", "Status 200", "Notes"]
-    x.align = "l"
+    table = PrettyTable()
+    table.field_names = ["URL", "Domain", "Prepaid for a month", "Status 200", "Notes"]
+    table.align = "l"
 
     for url in domains_list:
         notes = []
@@ -53,6 +55,6 @@ if __name__ == '__main__':
             notes.append("Invalid schema")
             is_status_200 = False
 
-        x.add_row([url, domain, expiration_date_info, 'Yes' if is_status_200 else 'No', '\n'.join(notes)])
+        table.add_row([url, domain, expiration_date_info, 'Yes' if is_status_200 else 'No', '\n'.join(notes)])
 
-    print(x)
+    print(table)
